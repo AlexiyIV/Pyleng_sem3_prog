bb8a7c030105d23f858d1c5bb23875b140a0ce30
Сучков Алексей <AlexiySV@yandex.ru>
2022-12-23 15:02:26 +0300

    task2

---
+++ task2.py
@@ -0,0 +1,8 @@
+array1 = [int(i) for i in input('Введите элементы через пробел ').split()]
+array2 = []
+for i in range(len(array1)//2):
+    array2.append(array1[i] * array1[len(array1)-1-i])
+if len(array1) % 2 !=0:
+    array2.append(array1[len(array1)//2] ** 2)
+print(array1)
+print(array2)
